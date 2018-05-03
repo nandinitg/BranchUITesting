e5f7435a217ede2ce0ee1997bf6687608c47520a
nandinitg <ntaraganahalli@apple.com>
2018-05-02 21:35:46 -0700

    added UItest.java

---
+++ src/test/java/tests/UITest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import pages.HomePage;
+
+public class UITest extends BaseTest {
+
+@Test(priority = 0, description= "Verify the links in the Homepage")
+	public void verifyAllLinks() throws InterruptedException {
+	
+	HomePage homePage = new HomePage(driver, wait);
+	homePage.navigateToBranchWebsite();
+	homePage.verifyLinks();
+	}
+	
+}
